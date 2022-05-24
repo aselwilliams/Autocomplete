c45eaabc73ea0df5ef502045b9df7c13c388997e
aselwilliams <aselwilliams22@gmail.com>
2022-05-24 16:09:21 -0400

    filtered suggestions

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -4,9 +4,23 @@ function Autocomplete() {
     const [filteredSuggestions,setFilteredSuggestions]= useState([]);
     const [activeSuggestionIndex,setActiveSuggestionIndex]= useState(0);
     const [showSuggestions, setShowSuggestions] = useState(false);
-    const [input,setInput] =useState('')
+    const [input,setInput] =useState('');
+
+    const onChange=(e)=>{
+        const userInput=e.target.value;
+        //filter suggestions that don't contain the user's input
+        const unLinked=suggestions.filter((suggestion)=>suggestion.toLowerCase().indexOf(userInput.toLowerCase())>-1)
+      setInput(e.target.value);
+      setFilteredSuggestions(unLinked);
+      setActiveSuggestionIndex(0);
+      setShowSuggestions(true);
+    };
+    
   return (
-    <div>Autocomplete</div>
+   <>
+   <Input type='text' onChange={onChange} onKewDown={onKeyDown} value={input} />
+   {showSuggestions && input && <SuggestionsListComponent />}
+   </>
   )
 }
 
